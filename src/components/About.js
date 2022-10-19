e7d07a15eff9726e724cab3cb91ad9e00ab3374a
Matías Vega <elmativega3@gmail.com>
2022-10-19 01:59:25 -0300

    deploy Portafolio

---
+++ src/components/About.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section id="about">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            Hola 👋 soy Matías.
+            <br className="hidden lg:inline-block" />Full Stack Web Developer.
+          </h1>
+          <p className="mb-8 leading-relaxed">
+          Soy de una pequeña ciudad de Argentina, desde hace 15 años trabajo en un servicio de emergencias médicas como Chofer de Ambulancias, en esta hermosa profesión como Técnico Superior en Emergencia Médica he aprendido a desarrollar habilidades como trabajar en equipo, el liderazgo, toma de decisiones en distintas situaciones y contextos.
+          Me gusta hacer deportes y disfruto mucho las actividades al aire libre.
+          El mundo IT siempre me llamó la atención y como todo niño curioso he decidido dar un giro profesional.
+          No dejo de asombrarme en cada nuevo paso que doy, me motiva mucho. Tengo buena predisposición de aprender nuevas tecnologías para seguir creciendo.
+          </p>
+          <div className="flex justify-center">
+            <a
+              href="#contact"
+              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+              Contactame
+            </a>
+            <a 
+            
+              href="./CV Matías Vega.pdf" 
+              target="_blank"
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg ">
+              Mi CV      
+            </a>
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center rounded"
+            alt="hero"
+            src="./MyMV.jpeg"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,64 @@
+export const projects = [
+  {
+    title: "Lista de tareas diarias",
+    subtitle: "HTML, CSS y Javascripts",
+    description:
+      "Desarrollo de una APP de lista de tareas con responsive web design y con las funcionales de agregar, tachar, completar y eliminar tareas yla implementación de local Storage que sirve para guardar el registro de los datos",
+    image: "./lista.jpg",
+    link: "https://lista-de-tares.netlify.app",
+  },
+  {
+    title: "Memorizar",
+    subtitle: "HTML, CSS y Javascripts",
+    description:
+      "Es un pequeño juego donde se pone a pruebas la capacidad de memorizar imagenes.",
+    image: "./memoria.jpg",
+    link: "https://memorizar.netlify.app",
+  },
+  {
+    title: "PokemonApp",
+    subtitle: "React, Node.js y SQl",
+    description:
+      "Desarrollo de una SPA Responsive Design (Single Page Application) utilizando React para el Front End y Redux como state management. Todos los componentes fueron realizados con CSS sin el uso de librerías externas. La SPA consume datos de una API externa a través de un Back End desarrollado en Node.js utilizando Express, agregando nuevas funcionalidades a la API original. Funcionalidades del proyecto: Formulario controlado para la creación de un nuevo Pokemon, ordenamientos y filtros, paginación.",
+    image: "./pokemonApp.png",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "E-commerce Heatly food",
+    subtitle: "Muchas tecnologías",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+// export const testimonials = [
+//   {
+//     quote:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+//     image: "https://randomuser.me/api/portraits/men/1.jpg",
+//     name: "Jesse Hicks",
+//     company: "Zoozle",
+//   },
+//   {
+//     quote:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+//     image: "https://randomuser.me/api/portraits/men/94.jpg",
+//     name: "Ruben Alvarez",
+//     company: "Dooble.io",
+//   },
+// ];
+
+export const skills = [
+  "JavaScript",
+  "HTML",
+  "CSS",
+  "React",
+  "Redux",
+  "Nodejs",
+  "SQL",
+  "Material UI",
+  "Git","Github","NPM","Postgres","MySQL","Sequelize","Postman","Vercel","Netlify","Slack", "Discord",
+  "SCRUM"
+];
