f3312c72e8a307a83c89c2d6f9df67b1a3669e5b
Matías Vega <elmativega3@gmail.com>
2022-11-14 17:01:39 -0300

    saco telefono

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -23,7 +23,7 @@ export default function Contact() {
       .then(() => alert("Message sent!"))
       .catch((error) => alert(error));
   }
-
+const{WHATSAPP}=process.env;
   return (
     <section id="contact" className="relative">
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
@@ -39,26 +39,8 @@ export default function Contact() {
             style={{ filter: "opacity(0.7)" }}
             src="https://www.google.com/maps/embed/v1/place?q=2100+San+Martin+Esperanza+Santa+Fe+Argentina&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
           />
-          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
-            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                EMAIL
-              </h2>
-              <a className="text-indigo-400 leading-relaxed">
-              mvfullstackdeveloper@email.com
-              </a>
-              {/* <a
-             href="mailto:mvfullstackdeveloper@gmail.com"
-             target="_blank"
-             className="text-indigo-400 leading-relaxed"
-             >mvfullstackdeveloper@email.com</a> */}
-              <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
-               Telefono
-              </h2>
-              <p className="leading-relaxed"><a href="https://wa.me/543496534323" target="_blank">543496534323</a></p>
-            </div>
-          </div>
-        </div>
+          
+        </div> 
         <form
           netlify
           name="test"
